7a548a18ce4d17dd400e675685024b28524b2b6b
rebeccachemata13 <80950540+rebeccachemata13@users.noreply.github.com>
2022-06-06 13:31:44 +0200

    Color and Price change

--- color_controller.js
+++ color_controller.js
@@ -1,18 +1,34 @@
 let middle_image = document.getElementById("middle_image");
+let green_pyramide = document.getElementById("green_pyramide");
+let red_pyramide = document.getElementById("red_pyramide");
+let yellow_pyramide = document.getElementById("yellow_pyramide");
+let price = document.getElementById("price");
 
 function changeToRed(){
-    middle_image.getAttribute("src") = "images/diamonds_pyramide_red.png"        
-
+    middle_image.src = "images/diamonds_pyramide_red.png";
+    red_pyramide.classList.add("active_pyramide");
+    yellow_pyramide.classList.remove("active_pyramide");
+    green_pyramide.classList.remove("active_pyramide");
+    price.innerHTML="30 000 $";
 }
-
 function changeToGreen(){
-    console.log("hello");
-    middle_image.getAttribute("src") = "images/diamonds_pyramide_green.png"        
-
+    middle_image.src = "images/diamonds_pyramide_green.png";
+    green_pyramide.classList.add("active_pyramide");
+    yellow_pyramide.classList.remove("active_pyramide"); 
+    red_pyramide.classList.remove("active_pyramide");
+    price.innerHTML="45 000 $";   
+           
+}
+function changeToYellow(){
+    middle_image.src = "images/diamonds_pyramide_gelb.png";
+    yellow_pyramide.classList.add("active_pyramide"); 
+    red_pyramide.classList.remove("active_pyramide"); 
+    green_pyramide.classList.remove("active_pyramide");
+    price.innerHTML="25 000 $";  
+        
 }
 
-let green_pyramide = document.getElementById("green_pyramide");
-green_pyramide.addEventListener("click",changeToGreen);
 
-let red_pyramide = document.getElementById("green_pyramide");
+green_pyramide.addEventListener("click",changeToGreen);
 red_pyramide.addEventListener("click",changeToRed);
+yellow_pyramide.addEventListener("click",changeToYellow);

0ae63256b92e798bdd2be22c5b7906714f1420dd
rebeccachemata13 <80950540+rebeccachemata13@users.noreply.github.com>
2022-06-17 21:34:51 +0200

    Changing Names

--- scripts/color_controller.js
+++ scripts/color_controller.js
@@ -1,61 +1,6 @@
 let price_div_2 = document.getElementById("this_is_price_div");
 let price = document.createElement("price");
 
-
-
-console.log("dieses Script läuft");
-
-// function changeToRed(){
-//     middle_image.src = "images/diamonds_pyramide_red.png";
-//     red_pyramide.classList.add("active_pyramide");
-//     yellow_pyramide.classList.remove("active_pyramide");
-//     green_pyramide.classList.remove("active_pyramide");
-//     price.innerHTML="30 000 $";
-// }
-// function changeToGreen(){
-//     middle_image.src = "images/diamonds_pyramide_green.png";
-//     green_pyramide.classList.add("active_pyramide");
-//     yellow_pyramide.classList.remove("active_pyramide"); 
-//     red_pyramide.classList.remove("active_pyramide");
-//     price.innerHTML="45 000 $";   
-           
-// }
-// function changeToYellow(){
-//     middle_image.src = "images/diamonds_pyramide_gelb.png";
-//     yellow_pyramide.classList.add("active_pyramide"); 
-//     red_pyramide.classList.remove("active_pyramide"); 
-//     green_pyramide.classList.remove("active_pyramide");
-//     price.innerHTML="25 000 $";  
-        
-// }
-
-
-
-    // console.log("do somethiiiiiiiing");
-    // let green_pyramide = document.getElementById("slick-slide22");
-    // let red_pyramide = document.getElementById("slick-slide21");
-    // let yellow_pyramide = document.getElementById("slick-slide20");
-//     let dot1 = document.getElementById("slick-slide-control20");
-//     let dot2 = document.getElementById("slick-slide-control21");
-//     let dot3 = document.getElementById("slick-slide-control22");
-    
-    
-//     if(green_pyramide.classList.contains("slick-current")){
-//         price.innerHTML="45 000 $";     
-//     }
-//     if(red_pyramide.classList.contains("slick-current")){
-//         price.innerHTML="30 000 $";
-//     }
-//     if(yellow_pyramide.classList.contains("slick-active")){
-//        price.innerHTML="25 000 $";
-//     }
-//     price_div_2.appendChild(price);
-//     price.classList.add("second-headline");
-   
-// }
-
-
-
 function declaringDots() {
     let dot1 = document.getElementById("slick-slide-control20");
     let dot2 = document.getElementById("slick-slide-control21");
@@ -64,11 +9,16 @@ function declaringDots() {
     let produkt_info_1 = document.createElement("p");
     let produkt_info_2 = document.createElement("p");
     let produkt_info_div = document.getElementById("produkt_info_div");
+    let produkt_name = document.createElement("p");
+    let product_name_div = document.getElementById("product_name_div");
 
     function priceControllerRed(){
         price.innerHTML="30 000 $";
+        produkt_name.innerHTML = "HARIM RED RUBY";
         price_div_2.appendChild(price);
+        product_name_div.appendChild(produkt_name);
         price.classList.add("second-headline");
+        produkt_name.classList.add("first-headline");
 
         produkt_info_1.innerHTML = "Die Gucci Egypt Parade Kollektion setzt sich auf zeitgemäße Weise mit dem alten Ägypten außeinander und modernisiert die antike Art.\nErstellt aus den kostbarsten glänzendsten Edelsteinen, sind die Gucci Harim Pyramiden ein Hingucker in jeder Umgebung.";
         produkt_info_2.innerHTML = "Die Harim Red Ruby besteht hauptsächlich aus roten Rubinen und soll mit ihren einzigartigen Rottönen das Rote Meer symbolisieren.\nMit einer Größe von 50cm bringt sie jedem Besitzer ein Stück ägyptische Kultur in das Haus.";
@@ -90,8 +40,11 @@ function declaringDots() {
     }
     function priceControllerGreen(){
         price.innerHTML="45 000 $";
+        produkt_name.innerHTML = "HARIM GREEN EMERALD";
+        product_name_div.appendChild(produkt_name);
         price_div_2.appendChild(price);
         price.classList.add("second-headline");
+        produkt_name.classList.add("first-headline");
 
         produkt_info_1.innerHTML = "Die Gucci Egypt Parade Kollektion setzt sich auf zeitgemäße Weise mit dem alten Ägypten außeinander und modernisiert die antike Art.\nErstellt aus den kostbarsten glänzendsten Edelsteinen, sind die Gucci Harim Pyramiden ein Hingucker in jeder Umgebung.";
         produkt_info_2.innerHTML = "Die Harim Green Emerald besteht hauptsächlich aus grünen Smaragden und soll mit ihren einzigartigen Grüntönen die Vegetation um den Nil widerspiegeln.\nMit einer Größe von 50cm bringt sie jedem Besitzer ein Stück ägyptische Kultur in das Haus.";
@@ -102,8 +55,11 @@ function declaringDots() {
     }
     function priceControllerYellow(){
         price.innerHTML="25 000 $";
+        produkt_name.innerHTML = "HARIM YELLOW SAPHIRE";
+        product_name_div.appendChild(produkt_name);
         price_div_2.appendChild(price);
         price.classList.add("second-headline");
+        produkt_name.classList.add("first-headline");
 
         produkt_info_1.innerHTML = "Die Gucci Egypt Parade Kollektion setzt sich auf zeitgemäße Weise mit dem alten Ägypten außeinander und modernisiert die antike Art.\nErstellt aus den kostbarsten glänzendsten Edelsteinen, sind die Gucci Harim Pyramiden ein Hingucker in jeder Umgebung.";
         produkt_info_2.innerHTML = "Die Harim Yellow Saphire besteht hauptsächlich aus gelben Saphiren und soll mit ihren einzigartigen Gelbtönen die Sahara Wüste widerspiegeln.\nMit einer Größe von 50cm bringt sie jedem Besitzer ein Stück ägyptische Kultur in das Haus.";
@@ -131,12 +87,3 @@ function declaringDots() {
     priceControllerYellow();
 }
 setTimeout(declaringDots, 500);
-
-
-
-
-
-
-//  green_pyramide.addEventListener("click",changeToGreen);
-//  red_pyramide.addEventListener("click",changeToRed);
-//  yellow_pyramide.addEventListener("click",changeToYellow);
